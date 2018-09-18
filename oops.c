b4dce616bbd0b8da99b755bb25a14db5e4c12610
Ena Nayak <20nayak@stfrancishs.org>
2018-09-18 17:24:38 +0000

    init

--- oops.c
+++ oops.c
@@ -1,22 +1,26 @@
-
 #include <stdio.h>
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
     while (k<5)
     {
-        sum+=k; //use something different in "while" version
-    }
-
-    printf ("%d\n",sum);
-
-    k=5;
-
-    for (k=0;k<5;k++) {
-        k=k-1; //use something different in "for" version
         printf ("%d\n", k);
+        sum = sum + k; //use something different in "while" version
+        k++;
     }
+
+    printf ("Sum - %d\n",sum);
+
+    //k=5;
+
+    for (k=10;k>0;k--)
+    {
+       // k=k-1; //use something different in "for" version
+        printf ("%2d\n", k);
+    }
+
+    printf ("Blastoff!\n");
 
 }
